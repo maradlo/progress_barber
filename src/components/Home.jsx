a5f32895c10412db2e8be3299b1eafe3cc6a890f
Marek Žáčik <zacik@hopintaxi.com>
2024-10-17 12:13:48 +0200

    remove stahujeme sa text

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,41 +1,52 @@
-import React from 'react';
-import TypedText from './TypedText';
-import background from '../assets/Background.png';
+import React from "react";
+import TypedText from "./TypedText";
+import background from "../assets/Background.png";
 
 const Home = () => {
   const openProgressBookio = () => {
     window.open("https://services.bookio.com/progressbarber/widget", "_blank");
-  }
+  };
 
   const openAndyBookio = () => {
-    window.open("https://services.bookio.com/progressbarber-and-coll/widget", "_blank");
-  }
+    window.open(
+      "https://services.bookio.com/progressbarber-and-coll/widget",
+      "_blank"
+    );
+  };
 
   return (
-    <section id="home" className="h-screen w-screen bg-cover" style={{ backgroundImage: `url(${background})` }}>
+    <section
+      id="home"
+      className="h-screen w-screen bg-cover"
+      style={{ backgroundImage: `url(${background})` }}
+    >
       <div className="text-center pt-36">
-        <div className="text-4xl lg:text-6xl mt-16 font-bold text-white">ProgressBarber</div>
+        <div className="text-4xl lg:text-6xl mt-16 font-bold text-white">
+          ProgressBarber
+        </div>
         <div className="mt-4">
           <TypedText />
         </div>
-        <div className="text-2xl mt-16 text-white">
-          Objednaj sa k:
-        </div>
+        <div className="text-2xl mt-16 text-white">Objednaj sa k:</div>
         <div className="flex space-x-16 justify-center mt-6">
           <div>
-            <button onClick={() => openProgressBookio()} type='button' className="text-2xl ml-4 px-8 py-2 font-bold text-white rounded transition duration-300 ease-in-out focus:outline-none focus:shadow-outline bg-orange-500 border border-orange-500 hover:bg-orange-700 hover:border-orange-700">ProgressBarber</button>
+            <button
+              onClick={() => openProgressBookio()}
+              type="button"
+              className="text-2xl ml-4 px-8 py-2 font-bold text-white rounded transition duration-300 ease-in-out focus:outline-none focus:shadow-outline bg-orange-500 border border-orange-500 hover:bg-orange-700 hover:border-orange-700"
+            >
+              ProgressBarber
+            </button>
           </div>
           <div>
-          <button onClick={() => openAndyBookio()} type='button' className="text-2xl mr-4 px-8 py-2 font-bold text-white rounded transition duration-300 ease-in-out focus:outline-none focus:shadow-outline bg-red-500 border border-red-500 hover:bg-red-700 hover:border-red-700">AndyBarber</button>
+            <button
+              onClick={() => openAndyBookio()}
+              type="button"
+              className="text-2xl mr-4 px-8 py-2 font-bold text-white rounded transition duration-300 ease-in-out focus:outline-none focus:shadow-outline bg-red-500 border border-red-500 hover:bg-red-700 hover:border-red-700"
+            >
+              AndyBarber
+            </button>
           </div>
-        </div>
-        <div className="flex space-x-16 justify-center md:mt-12 mt-6">
-          <span className='font-extrabold text-orange-500 text-2xl md:text-8xl'>Sťahujeme sa!</span>
-        </div>
-        <div className="grid justify-center md:mt-4 mt-2">
-          <span className='font-normal text-lg md:text-2xl text-white'>Od 1.7.2024 na novej adrese</span>
-          <span className='font-normal mt-2 text-lg md:mt-4 md:text-2xl text-white'>Nám. Sv. Juraja, č. 2244</span>
-          <span className='font-normal mt-2 text-lg md:text-xl text-gray-300'>(Vedľa 365 banky)</span>
         </div>
       </div>
     </section>
